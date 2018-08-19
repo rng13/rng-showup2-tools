3a7b0621b1d35698eee61d41d7e3fc7d9eaf5b1f
rng13 <42441028+rng13@users.noreply.github.com>
2018-08-19 23:37:14 +0200

    Update napiwki-menu-kwiat13.user.js

--- napiwki-menu-kwiat13.user.js
+++ napiwki-menu-kwiat13.user.js
@@ -11,26 +11,52 @@
 // ==/UserScript==
 
 (function () {
-    let zmiany = [
-        {co: 20, naco: 100, opis: "100"},
-        {co: 35, naco: 25, opis: "klpsy"},
-        {co: 50, naco: 30, opis: "cycki"},
-        {co: 75, naco: 50, opis: "hiszp"},
-        {co: 100, naco: 70, opis: "lubię"}
-    ];
-    let elementy = [];
-    zmiany.forEach(function ({co, naco, opis}) {
-        let elem = document.querySelector('#tip-form button[data-coin="' + co + '"]');
-        if (elem) {
-            elementy.push({
-                elem: elem,
-                naco: naco,
-                opis: opis
-            });
-        }
-    });
-    elementy.forEach(function ({elem, naco, opis}) {
-        elem.innerHTML = opis;
-        elem.dataset.coin = naco;
-    });
+    let trzeba_podmienic = false;
+    if (window.location.pathname === '/kwiat13') {
+        trzeba_podmienic = true;
+    } else {
+        // jestesmy w grupowej wiec trzeba sprawdzic czy u kwiatka
+        let lll = document.querySelectorAll('.mod-username');
+        trzeba_podmienic = lll[0].text === 'kwiat13';
+    }
+    if (trzeba_podmienic) {
+        let zmiany = [
+            {co: 20, naco: 100, opis: "100"},
+            {co: 35, naco: 11, opis: "11"},
+            {co: 50, naco: 25, opis: "klpsy"},
+            {co: 75, naco: 30, opis: "cycki"},
+            {co: 100, naco: 50, opis: "hiszp"}
+        ];
+        let dodatkowe = [
+            {naco: 69, opis: "cipka"},
+            {naco: 70, opis: "lubię"},
+            {naco: 111, opis: "tańcz"}
+        ];
+        let elementy = [];
+        zmiany.forEach(function ({co, naco, opis}) {
+            let elem = document.querySelector('#tip-form button[data-coin="' + co + '"]');
+            if (elem) {
+                elementy.push({
+                    elem: elem,
+                    naco: naco,
+                    opis: opis
+                });
+            }
+        });
+        elementy.forEach(function ({elem, naco, opis}) {
+            elem.innerHTML = opis;
+            elem.dataset.coin = naco;
+        });
+        let refElement = elementy[elementy.length - 1].elem;
+        dodatkowe.forEach(function ({naco, opis}) {
+            let btn = document.createElement('button');
+            btn.setAttribute('type', 'button');
+            btn.className = 'btn btn--coin';
+            btn.dataset.coin = naco;
+            btn.innerHTML = opis;
+            refElement.parentNode.insertBefore(btn, refElement.nextSibling);
+            refElement = btn;
+            // <button type="button" class="btn btn--coin" data-coin="15">15</button>
+        });
+    }
 })();
